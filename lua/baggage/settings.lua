fe7b14b187988ee31733b48f08472898e06dd654
Nils Riedemann <nils@bleepbloop.studio>
2023-09-08 21:20:12 +0200

    feat: raise error if vim.g.baggage_path is not set

--- lua/baggage/settings.lua
+++ lua/baggage/settings.lua
@@ -1,6 +1,7 @@
 ---@class BaggageSettings
 local M = {
-  pack_path = vim.g.baggage_path
+  pack_path = vim.g.baggage_path or error("vim.g.baggage_path is not set!"),
+  opt_path = vim.g.baggage_path .. 'opt/',
 }
 
 return M
