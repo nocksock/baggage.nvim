c6134ed9ecf730d0b3bbec7fe5978882f86949ae
Nils Riedemann <nils@bleepbloop.studio>
2024-12-14 23:14:31 +0100

    fix: use vim.uv instead of deprecated vim.loop

--- lua/baggage/from_remote.lua
+++ lua/baggage/from_remote.lua
@@ -2,16 +2,16 @@ local r = require
 ---@param source string
 ---@param opts PluginOptions
 return function(source, opts)
-  local plugin   = r'baggage.create_plugin_from_url' (source, opts)
+  local plugin = r 'baggage.create_plugin_from_url' (source, opts)
 
-  r'baggage.prepare_opt_directory'(plugin)
-  if not vim.loop.fs_stat(plugin.path) then
-    r'baggage.within'(r'baggage.settings'.opt_path, function()
+  r 'baggage.prepare_opt_directory' (plugin)
+  if not vim.uv.fs_stat(plugin.path) then
+    r 'baggage.within' (r 'baggage.settings'.opt_path, function()
       -- using vim.cmd because vim.fn.system also blocks the UI when running synchroneously so we
       -- can't print info messages. But we *have* to run synchrounously because code after the call to
       -- from relies on packages existing.
       if plugin.commit then
-        vim.cmd("!git submodule add --branch ".. plugin.commit .. " " .. plugin.clone_url .. " " .. plugin.dirname)
+        vim.cmd("!git submodule add --branch " .. plugin.commit .. " " .. plugin.clone_url .. " " .. plugin.dirname)
       else
         vim.cmd("!git submodule add " .. plugin.clone_url .. " " .. plugin.dirname)
       end
@@ -19,7 +19,7 @@ return function(source, opts)
       vim.cmd("!git commit -m 'add: " .. plugin.dirname .. "'")
     end)
 
-    r'baggage.within'(plugin.path, function()
+    r 'baggage.within' (plugin.path, function()
       if plugin.opts.on_sync then
         if vim.startswith(plugin.opts.on_sync, ":") then
           vim.cmd(plugin.opts.on_sync:sub(2))
@@ -28,7 +28,7 @@ return function(source, opts)
         end
       end
 
-      if vim.loop.fs_stat(plugin.path .. "/doc") then
+      if vim.uv.fs_stat(plugin.path .. "/doc") then
         vim.cmd("helptags " .. plugin.path .. "/doc")
       end
     end)
